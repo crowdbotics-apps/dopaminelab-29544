4c63698cdc33af412c0c39a5cccc27d5ca8e9164
Crowdbotics <team@crowdbotics.com>
2021-08-06 02:01:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dopaminelab_29544Identifier",
+  url: "https://dopaminelab_29544Identifier.botics.co",
+  api: "https://dopaminelab_29544Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
